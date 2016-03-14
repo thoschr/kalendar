ce3d6e0a6935002ed0755be80935b55c2db4e09e
echo-devim <devim@outlook.it>
2016-03-14 17:37:44 +0100

    Minor tests fixes

--- test/test.cpp
+++ test/test.cpp
@@ -7,12 +7,15 @@ Test::Test()
 
 void Test::test_all() {
     test_persistence();
+    test_util();
 }
 
 void Test::test_persistence() {
     print("\nStarting persistence tests\n");
     PManagerTest pmt;
     pmt.test_all();
+}
+void Test::test_util() {
     print("\nStarting util tests\n");
     TimeUtilTest tut;
     tut.test_all();
--- test/test.h
+++ test/test.h
@@ -17,6 +17,7 @@ public:
     Test();
     void test_all();
     void test_persistence();
+    void test_util();
     /* fflush needed to see the output on the Application Output of QT Creator */
     static void print_green(string s) { printf("\033[32m%s\033[39m", s.c_str()); fflush(stdout); }
     static void print_red(string s) { printf("\033[31m%s\033[39m", s.c_str()); fflush(stdout); }
