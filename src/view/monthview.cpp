b305c75b59abefa958213b089eddc6e0d5797a09
echo-devim <devim@outlook.it>
2016-03-16 13:55:54 +0100

    Fixed first weekday count

--- src/view/monthview.cpp
+++ src/view/monthview.cpp
@@ -16,28 +16,29 @@
                    "QLabel { border:none; }" \
                    "QFrame#today { background-color: #FFFF66; }"
 
-/* This is just a draft to get an idea */
 
 MonthView::MonthView(QWidget *parent) :
     QMainWindow(parent),
     ui(new Ui::MonthView)
 {
-    Time time = TimeUtil::get_current_time();
-    int tot_days = TimeUtil::get_days_in_month(time.getMonth(), time.getYear());
-    QLabel *label_date = new QLabel(QString(TimeUtil::get_literal_month(time.getMonth()).c_str()) + QString(" ") + QString::number(time.getYear()));
+    Time current_time = TimeUtil::get_current_time();
+    int tot_days = TimeUtil::get_days_in_month(current_time.getMonth(), current_time.getYear());
+    QLabel *label_date = new QLabel(QString(TimeUtil::get_literal_month(current_time.getMonth()).c_str()) + QString(" ") + QString::number(current_time.getYear()));
     QGridLayout *grid_layout = new QGridLayout;
     QVBoxLayout *layout = new QVBoxLayout;
     label_date->setMaximumHeight(20);
     layout->addWidget(label_date);
     //Create 7x7 grid
-    int i,j,x;
+    int i,j,x,start_wday;
+    //weekday of the first day of the current month
+    start_wday = current_time.getWeekDay() - (current_time.getMonthDay() % 7) + 1;
     x = 1;
     for (i = 0; i < 7; i++) {
         for (j = 0; j < 7; j++) {
             QFrame *frame = new QFrame;
             QVBoxLayout *vl = new QVBoxLayout;
-            if (((i > 0) || (j >= time.getWeekDay()-1)) && (x <= tot_days)) {
-                if (x == time.getMonthDay())
+            if (((i > 0) || (j >= start_wday-1)) && (x <= tot_days)) {
+                if (x == current_time.getMonthDay())
                     frame->setObjectName("today");
                 QLabel *day = new QLabel(QString::number(x));
                 vl->addWidget(day);
@@ -58,6 +59,8 @@ MonthView::MonthView(QWidget *parent) :
      // Set layout in QWidget
      QWidget *window = new QWidget();
      window->setLayout(layout);
+     window->setMinimumHeight(600);
+     window->setMinimumWidth(1000);
 
      // Set QWidget as the central layout of the main window
      setCentralWidget(window);
