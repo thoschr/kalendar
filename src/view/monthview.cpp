014a4c0666f49f8fbfa9e79f4b3a558287181db6
echo-devim <devim@outlook.it>
2016-03-17 09:03:41 +0100

    Fixed the style of the labels

--- src/view/monthview.cpp
+++ src/view/monthview.cpp
@@ -13,8 +13,9 @@
 #include <QFrame>
 
 #define CELL_STYLE "QFrame { background-color: #FFFFFF; border: 1px solid #555555; }" \
-                   "QLabel { border:none; }" \
-                   "QFrame#today { background-color: #FFFF66; }"
+                   "QLabel { border: none; font-size: 16px; padding: 5px; }" \
+                   "QLabel#today { background-color: #FFFF66; }" \
+                   "QLabel#header { background-color: #EEEEEE; padding: auto; font-weight: bold; }"
 
 
 MonthView::MonthView(QWidget *parent) :
@@ -30,22 +31,41 @@ MonthView::MonthView(QWidget *parent) :
     layout->addWidget(label_date);
     //Create 7x7 grid
     int i,j,x,start_wday;
+    //First row contains the names of the days of the week
+    for (j = 0; j < 7; j++) {
+        QFrame *frame = new QFrame;
+        QVBoxLayout *vl = new QVBoxLayout;
+        frame->setMinimumWidth(150);
+        frame->setMinimumHeight(100);
+        QLabel *wday_name = new QLabel(TimeUtil::numeric2literal_day_of_week(j+1).c_str());
+        wday_name->setObjectName("header");
+        vl->addWidget(wday_name);
+        vl->setMargin(0);
+        frame->setLayout(vl);
+        frame->setStyleSheet(CELL_STYLE);
+        grid_layout->addWidget(frame, 0, j);
+    }
     //weekday of the first day of the current month
     start_wday = current_time.getWeekDay() - (current_time.getMonthDay() % 7) + 1;
     x = 1;
-    for (i = 0; i < 7; i++) {
+    for (i = 1; i < 7; i++) {
         for (j = 0; j < 7; j++) {
             QFrame *frame = new QFrame;
             QVBoxLayout *vl = new QVBoxLayout;
-            if (((i > 0) || (j >= start_wday-1)) && (x <= tot_days)) {
-                if (x == current_time.getMonthDay())
-                    frame->setObjectName("today");
+            if (((i > 1) || (j >= start_wday-1)) && (x <= tot_days)) {
                 QLabel *day = new QLabel(QString::number(x));
+                if (x == current_time.getMonthDay())
+                    day->setObjectName("today");
+                else
+                    day->setObjectName("day");
+                day->setMaximumHeight(25);
+                vl->setAlignment(Qt::AlignTop | Qt::AlignRight);
                 vl->addWidget(day);
+                vl->setMargin(0);
                 x++;
             }
-            frame->setMinimumWidth(60);
-            frame->setMinimumHeight(60);
+            frame->setMinimumWidth(150);
+            frame->setMinimumHeight(100);
             frame->setLayout(vl);
             frame->setStyleSheet(CELL_STYLE);
             grid_layout->addWidget(frame, i, j);
@@ -59,8 +79,8 @@ MonthView::MonthView(QWidget *parent) :
      // Set layout in QWidget
      QWidget *window = new QWidget();
      window->setLayout(layout);
-     window->setMinimumHeight(600);
-     window->setMinimumWidth(1000);
+     window->setMinimumHeight(700);
+     window->setMinimumWidth(1200);
 
      // Set QWidget as the central layout of the main window
      setCentralWidget(window);
