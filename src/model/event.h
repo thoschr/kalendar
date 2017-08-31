5af06a1ad6c264aa4a39d7f9e174abace0960efd
echo-devim <devim@outlook.it>
2017-08-31 16:25:59 +0200

    Added support to 32 bit systems

--- src/model/event.h
+++ src/model/event.h
@@ -8,8 +8,15 @@
 /* A todo is defined as an event with special dates, they are the following:
  * start = end = 29/01/2105 09:40 , the UTC timestamp is: 4262665200
  * Note: this value is incompatible for 32bit systems due to the 2038 year bug (Reference: https://en.wikipedia.org/wiki/Year_2038_problem)
- * The todo is only an high-level concept managed by the view logic, thus the persistence part consider them as normal events. */
+ * The todo is only an high-level concept managed by the logic view, thus the persistence part consider them as normal events. */
+//#define BIT32
+#ifndef BIT32
+// As explained above, this is for 64 bit systems
 #define TODO_DATE 4262665200
+#else
+// 2147437133 is the 18 Jan 2038, the maximum value representable with a 32 bit system
+#define TODO_DATE 2147437133
+#endif
 
 using namespace std;
 
