ea88e40e75b58a8c6b816ba4015cba140f197185
greg <devim@outlook.it>
2018-05-24 21:34:56 +0100

    Implemented settings

--- src/view/settings.cpp
+++ src/view/settings.cpp
@@ -1,6 +1,50 @@
 #include "settings.h"
 
-settings::settings()
-{
+#include <QDebug>
 
+const QString SettingsValues::past_months_expiration = "PastMonthsExpiration";
+
+Settings::Settings(QWidget *parent) :
+    QDialog(parent),
+    ui(new Ui::Settings)
+{
+    this->setWindowFlags(Qt::WindowCloseButtonHint | Qt::WindowMinimizeButtonHint);
+    this->setWindowTitle("Settings");
+    QVBoxLayout *main_layout = new QVBoxLayout;
+    QHBoxLayout *row1 = new QHBoxLayout;
+    QLabel *lblPastMonthExpiration = new QLabel("Delete all events older than (months): ");
+    this->pastMonthsExpiration = new QSpinBox;
+    pastMonthsExpiration->setRange(0, 120);
+    pastMonthsExpiration->setSingleStep(1);
+    pastMonthsExpiration->setValue(2);
+    pastMonthsExpiration->setFixedWidth(100);
+    row1->addWidget(lblPastMonthExpiration);
+    row1->addWidget(pastMonthsExpiration);
+    QHBoxLayout *lastRow = new QHBoxLayout;
+    QPushButton *btnCancel = new QPushButton("Cancel");
+    QPushButton *btnSave = new QPushButton("Save");
+    connect(btnCancel, &QPushButton::clicked, this, &Settings::on_button_cancel_click);
+    connect(btnSave, &QPushButton::clicked, this, &Settings::on_button_save_click);
+    lastRow->addWidget(btnCancel);
+    lastRow->addWidget(btnSave);
+    main_layout->addLayout(row1);
+    main_layout->addLayout(lastRow);
+    setLayout(main_layout);
 }
+
+Settings::~Settings()
+{
+    delete ui;
+}
+
+void Settings::on_button_cancel_click() {
+   this->close();
+}
+
+void Settings::on_button_save_click() {
+    QSettings settings;
+    settings.setValue(SettingsValues::past_months_expiration, this->pastMonthsExpiration->value());
+    this->close();
+}
+
+
--- src/view/settings.h
+++ src/view/settings.h
@@ -1,11 +1,43 @@
 #ifndef SETTINGS_H
 #define SETTINGS_H
 
+#include "ui_settings.h"
+#include <QDialog>
+#include <QKeyEvent>
+#include <QLabel>
+#include <QSpinBox>
+#include <QVBoxLayout>
+#include <QHBoxLayout>
+#include <QPushButton>
+#include <QLayout>
+#include <QSettings>
+#include "../persistence/securepmanager.h"
 
-class settings
+namespace Ui {
+class Settings;
+}
+
+struct SettingsValues {
+    /* initialization in settings.cpp */
+    static const QString past_months_expiration;
+};
+
+class Settings : public QDialog
 {
+    Q_OBJECT
+
+private:
+    QSpinBox *pastMonthsExpiration;
+    Ui::Settings *ui;
+
 public:
-    settings();
+    explicit Settings(QWidget *parent = 0);
+    ~Settings();
+    void on_button_cancel_click();
+    void on_button_save_click();
+
+protected:
+
 };
 
 #endif // SETTINGS_H
